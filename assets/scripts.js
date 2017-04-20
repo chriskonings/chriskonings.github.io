fd001e82b104efc4c59d3789f0bc8f96156161f3
Chris Konings <chriskonings@CK.home>
2017-04-20 19:38:22 +0100

    added scripts file but commented scripts

---
+++ assets/scripts.js
@@ -0,0 +1,7 @@
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   var accordion = document.querySelector('.accordion');
+//   accordion.querySelector('.accordion__button').addEventListener("click", function(){
+//     var content = this.nextElementSibling;
+//     console.log(content);
+//   }, false);
+// });
